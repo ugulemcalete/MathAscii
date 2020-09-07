72f795ff944f0eb50422a485de1a0f494ba3b072
MrChazar <43088264+MrChazar@users.noreply.github.com>
2020-09-07 20:33:47 +0200

    kk

--- zad3.cpp
+++ zad3.cpp
@@ -1,3 +1,5 @@
+/*Napisz program, kt�ry poprosi u�ytkownika o podanie imienia i nazwiska a nast�pnie utworzy, zapisze i wy�wietli tzeci napis sk�adaj�cy si� z podanego nazwiska
+,przecinka,spacji, i imienia. U�yj obiektu string i metod z pliku nag��wkowego string*/
 #include <iostream>
 #include <string.h>
 #include <math.h>
@@ -5,6 +7,10 @@ using namespace std;
 
 int main()
 {
+
+    string name,surname;
+    cin >> name;
+
 
     return 0;
 }
